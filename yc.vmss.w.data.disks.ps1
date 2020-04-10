a44f405ff07a001fa881faa4b7369d079bb0cc4d
yungchou <yc2121@live.com>
2020-04-10 13:10:58 -0500

    v2

---
+++ yc.vmss.w.data.disks.ps1
@@ -0,0 +1,22 @@
+﻿# vmss with data disks
+
+az login
+
+rgNmae='myRG'
+vmssName='myVMSS'
+region='southcentralus'
+image='ubuntults'
+mvAdmin='alice'
+
+az group create -n $rgName --location $region
+
+az vmss create \
+  -g $rgName \
+  -n $vmssName \
+  --image $image \
+  --admin-username $vmAdmin \
+  --upgrade-policy automatic \
+  --generate-ssh-keys \
+  --data-disk-sizes-gb 5 5
+
+az group delete -n $rgName --no-wait --yes 
