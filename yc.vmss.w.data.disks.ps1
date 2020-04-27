de5035d3308cdd4f0bb0b12d912a7e4ccb2c5a8e
yungchou <yc2121@live.com>
2020-04-27 17:31:30 -0500

    vmss

--- yc.vmss.w.data.disks.ps1
+++ yc.vmss.w.data.disks.ps1
@@ -1,22 +1,50 @@
 ﻿# vmss with data disks
+# https://docs.microsoft.com/en-us/azure/virtual-machine-scale-sets/scripts/cli-sample-create-simple-scale-set?toc=/cli/azure/toc.json#sample-script
 
+# Session Start
 az login
+az account list -o table
 
-rgNmae='myRG'
-vmssName='myVMSS'
+# CUSTOMIZATION
+initial='yc'
+
+# Session Tag
+tag=$initial$(date +%M%S)
+
+rgName=$tag"rg"
 region='southcentralus'
-image='ubuntults'
-mvAdmin='alice'
+
+vmssName=$tag"vmss"
+adminID='alice'
+
+uImage='ubuntults'
+#w16Image='win2016datacenter'
+#w19Image='win2019datacenter'
+vmSize='Standard_B2ms'
 
 az group create -n $rgName --location $region
 
 az vmss create \
-  -g $rgName \
-  -n $vmssName \
-  --image $image \
-  --admin-username $vmAdmin \
-  --upgrade-policy automatic \
-  --generate-ssh-keys \
-  --data-disk-sizes-gb 5 5
+-g $rgName \
+-n $vmssName \
+-l $region \
+--app-gateway $vmss"-gateway" \
+--backend-pool-name $vmss"-be-pool" \
+--image $uImage \
+--authentication-type All \
+--generate-ssh-keys \
+--vm-sku $vmSize \
+--admin-username $adminID \
+--data-disk-sizes-gb 4 4 \
+--instance-count 5 \ # default 2
+--disable-overprovision $true \
+--eviction-policy Delete \
+--upgrade-policy Automatic \
+--scale-in-policy OldestVM \
+-o table
+
+# --customer-data cloud_init.yaml
+# --validate
+# --no-wait
 
 az group delete -n $rgName --no-wait --yes 
