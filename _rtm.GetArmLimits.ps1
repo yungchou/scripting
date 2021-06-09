8f1376149d374ba416cd6f7a26302358658f52a0
yungchou <yc2121@live.com>
2021-06-09 16:35:56 -0500

    udpate

--- _rtm.GetArmLimits.ps1
+++ _rtm.GetArmLimits.ps1
@@ -1,61 +1,55 @@
 <#
 .SYNOPSIS
 
-There are limits to the number of read/write operations that can be performed against the Azure Resource manager proviers in Azure. 
-When this limit is reached there will be an HTTP 429 error returned.  The documentation below outlines the specific REST call but
-does not provide a complete example
+There are limits to the number of read/write operations that can be performed against the Azure Resource manager proviers in Azure. When this limit is reached there will be an HTTP 429 error returned.  The documentation below outlines the specific REST call but does not provide a complete example
 
 https://docs.microsoft.com/en-us/azure/azure-resource-manager/resource-manager-request-limits
 
 .DESCRIPTION
 
-This script creates the proper bearer token to invoke the REST API on the number of remaining Read Operations allowed against a specific 
-subscription.  The function Get-AzureCachedAccessToken provides the logic to pull the access token required to pass into the REST API
-
+This script creates the proper bearer token to invoke the REST API on the number of remaining Read Operations allowed against a specific subscription.  The function Get-AzureCachedAccessToken provides the logic to pull the access token required to pass into the REST API
 
 REF:
 https://github.com/microsoft/csa-misc-utils
 
 #>
 
+function Get-AzCachedAccessToken {
+  <#
+    .SYNOPSIS
+    Get the Bearer token from the currently set AzContext
 
-function Get-AzureRmCachedAccessToken() {
-  $ErrorActionPreference = 'Stop'
-  
-  if (-not (Get-Module AzureRm.Profile)) {
-    Import-Module AzureRm.Profile
-  }
-  $azureRmProfileModuleVersion = (Get-Module AzureRm.Profile).Version
-  # refactoring performed in AzureRm.Profile v3.0 or later
-  if ($azureRmProfileModuleVersion.Major -ge 3) {
-    $azureRmProfile = [Microsoft.Azure.Commands.Common.Authentication.Abstractions.AzureRmProfileProvider]::Instance.Profile
-    if (-not $azureRmProfile.Accounts.Count) {
-      Write-Error "Ensure you have logged in before calling this function."    
-    }
-  }
-  else {
-    # AzureRm.Profile < v3.0
-    $azureRmProfile = [Microsoft.WindowsAzure.Commands.Common.AzureRmProfileProvider]::Instance.Profile
-    if (-not $azureRmProfile.Context.Account.Count) {
-      Write-Error "Ensure you have logged in before calling this function."    
-    }
-  }
-  
-  $currentAzureContext = Get-AzureRmContext
+    .DESCRIPTION
+    Get the Bearer token from the currently set AzContext. Retrieves from Get-AzContext
+
+    .EXAMPLE
+    Get-AzCachedAccesstoken
+
+    .EXAMPLE
+    Get-AzCachedAccesstoken -Verbose
+
+    #>
+  [cmdletbinding()]
+  param()
+
+  $currentAzureContext = Get-AzContext
+
+  $azureRmProfile = [Microsoft.Azure.Commands.Common.Authentication.Abstractions.AzureRmProfileProvider]::Instance.Profile
   $profileClient = New-Object Microsoft.Azure.Commands.ResourceManager.Common.RMProfileClient($azureRmProfile)
-  Write-Debug ("Getting access token for tenant" + $currentAzureContext.Subscription.TenantId)
-  $token = $profileClient.AcquireAccessToken($currentAzureContext.Subscription.TenantId)
+    
+  Write-Verbose ("Tenant: {0}" -f $currentAzureContext.Subscription.Name)
+    
+  $token = $profileClient.AcquireAccessToken($currentAzureContext.Tenant.TenantId)
   $token.AccessToken
-
 }
 
-
 Write-Host "Log in to your Azure subscription..." -ForegroundColor Green
-#Login-AzureRmAccount
-#Get-AzureRmSubscription -SubscriptionName $SubscriptionName | Select-AzureRmSubscription
+#Connect-AzAccount
+#$SubscriptionName='????'
+#Get-AzSubscription -SubscriptionName $SubscriptionName | Select-AzSubscription
 
-$token = Get-AzureRmCachedAccessToken
-$currentAzureContext = Get-AzureRmContext
+$token = Get-AzCachedAccessToken
+$currentAzureContext = Get-AzContext
 Write-Host ("Getting access ARM Throttle Limits for Subscription: " + $currentAzureContext.Subscription)
 
 
